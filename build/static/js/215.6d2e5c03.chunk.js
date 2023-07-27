c64faa0a73739950ee993d39c1ac8c1534b63051
prandeveloper <pranaydevelopersveltosest@gmail.com>
2023-07-27 18:59:21 +0530

    build

---
+++ build/static/js/215.6d2e5c03.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[215],{2413:function(a,s){}}]);
+//# sourceMappingURL=215.6d2e5c03.chunk.js.map
---
+++ build/static/js/220.f9c7409a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[220],{2396:function(a,s){}}]);
+//# sourceMappingURL=220.f9c7409a.chunk.js.map
